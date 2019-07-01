165de90ac1b37b4cb27e396b10efaabe54155ed4
huriya-geek <52177156+huriya-geek@users.noreply.github.com>
2019-07-01 17:41:51 +0530

    Update CRUD.php

--- CRUD.php
+++ CRUD.php
@@ -8,7 +8,7 @@
 			$iQuery = "INSERT INTO account(username,password) values(?,?)";
 
 			$stmt = $conn->prepare($iQuery);
-			$stmt->bind_param("ss","$username","$password");
+			$stmt->bind_param("ss",$username,$password);
 			if($stml->execute()){
 				$_SESSION['msg'] ="New Record is successfully inserted";
 				$_SESSION['alert'] ="alert alert-success";
@@ -22,5 +22,38 @@
 		}
 		header("location:index.php");
 	}
+	if(isset($_POST['delete'])){
+		$id = $_POST['delete'];
 
+		$dQuery = "DELETE from account where id = ?";
+		$stmt  = $conn->prepare($dQuery);
+		$stmt->bind_param('i',$id);
+		if($stml->execute()){
+				$_SESSION['msg'] ="New Record is successfully deleted";
+				$_SESSION['alert'] ="alert alert-success";
+			}
+			$stmt->close();
+			$conn->close();
+			header("location:index.php");
+		}
+	}
+	if(isset($_POST['edit'])){
+		if(!empty($_POST['username']) && !empty($_POST['password'])){
+			$username=$_POST['username'];
+			$password=$_POST['password'];
+			$id=$_POST['edit'];
+
+			$uQuery = "UPDATE account SET username=?, password=? where id=?";
+
+			$stmt = $conn->prepare($uQuery);
+			$stmt->bind_param("ssi",$username,$password,$id);
+			if($stml->execute()){
+				$_SESSION['msg'] ="New Record is successfully updated";
+				$_SESSION['alert'] ="alert alert-success";
+			}
+			$stmt->close();
+			$conn->close();
+			header("location:index.php");
+		}
+	}
 ?>
